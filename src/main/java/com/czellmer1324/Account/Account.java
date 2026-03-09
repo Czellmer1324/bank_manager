3e151404082cec98f64fdb0d3004485ebdab9e4b
Czellmer1324 <114022227+Czellmer1324@users.noreply.github.com>
2026-03-08 21:22:18 -0400

    Added basic stuff to account class

--- src/main/java/com/czellmer1324/Account/Account.java
+++ src/main/java/com/czellmer1324/Account/Account.java
@@ -1,5 +1,16 @@
 package com.czellmer1324.Account;
 
-public abstract class Account {
+import java.io.Serializable;
 
+public abstract class Account implements Serializable {
+    float balance;
+    int accountNumber;
+
+    public void deposit(float amount) {
+        balance+= amount;
+    }
+
+    public float viewBalance() {
+        return balance;
+    }
 }
